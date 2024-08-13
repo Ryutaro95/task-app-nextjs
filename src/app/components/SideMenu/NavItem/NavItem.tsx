9c566a9d37a92f6912693f6612930533c7d55f34
Ryutaro <ryutaro9595@gmail.com>
2024-08-14 04:42:20 +0900

    fix sidebar

--- src/app/components/SideMenu/NavItem/NavItem.tsx
+++ src/app/components/SideMenu/NavItem/NavItem.tsx
@@ -12,13 +12,22 @@ interface NavItemProps {
 const NavItem: React.FC<NavItemProps> = ({ label, link, icon }) => {
   const pathname = usePathname();
   return (
-    <Link
-      href={link}
-      className={`flex p-4 items-center w-full hover:bg-gray-700 font-medium ${pathname === link ? "bg-gray-600 border-r-4 border-r-green-500" : ""}`}
-    >
-      <div className="mr-1">{icon}</div>
-      <div>{label}</div>
-    </Link>
+    <div>
+      <div className="w-full flex items-center gap-x-1.5 group select-none">
+        <div className="w-1 rounded-xl h-8 bg-transparent transition-colors duration-200 relative overflow-hidden">
+          <div
+            className={`absolute top-0 left-0 w-full h-[102%]  group-hover:translate-y-0  bg-indigo-600 transition-all duration-300 ${pathname === link ? "translate-y-0" : "translate-y-full"}`}
+          ></div>
+        </div>
+        <Link
+          href={link}
+          className={`group-hover:bg-white/10 w-full group-active:scale-95 self-stretch pl-2 rounded flex items-center space-x-2 transition-all duration-200 dark:group-hover:text-white dark:hover:text-white text-sm ${pathname === link ? "bg-white/10 text-white" : ""}`}
+        >
+          <div>{icon}</div>
+          <span className="font-QuicksandMedium">{label}</span>
+        </Link>
+      </div>
+    </div>
   );
 };
 
--- src/app/components/SideMenu/NavList/NavList.tsx
+++ src/app/components/SideMenu/NavList/NavList.tsx
@@ -1,6 +1,10 @@
+"use client";
+
 import NavItem from "@/app/components/SideMenu/NavItem/NavItem";
+import { usePathname } from "next/navigation";
 import { FaRegCheckSquare, FaTasks } from "react-icons/fa";
 import { FaRegClock } from "react-icons/fa6";
+import { MdAddTask } from "react-icons/md";
 
 interface NavItemType {
   id: number;
@@ -10,37 +14,66 @@ interface NavItemType {
 }
 
 const NavList = () => {
+  const pathname = usePathname();
+
   const navList: NavItemType[] = [
     {
       id: 1,
       label: "All Tasks",
       link: "/",
-      icon: <FaTasks className="size-5" />,
+      icon: (
+        <FaTasks
+          className={`h-5 w-5 group-hover:fill-indigo-600 dark:fill-gray-600  transition-colors duration-200 ${pathname === "/" ? "!fill-indigo-500" : ""}`}
+        />
+      ),
     },
     {
       id: 2,
       label: "Completed Tasks",
       link: "/completed",
-      icon: <FaRegCheckSquare className="size-5" />,
+      icon: (
+        <FaRegCheckSquare
+          className={`h-5 w-5 group-hover:fill-indigo-600 dark:fill-gray-600  transition-colors duration-200 ${pathname === "/completed" ? "!fill-indigo-500" : ""}`}
+        />
+      ),
     },
     {
       id: 3,
       label: "Expired Tasks",
       link: "/expired",
-      icon: <FaRegClock className="size-5" />,
+      icon: (
+        <FaRegClock
+          className={`h-5 w-5 group-hover:fill-indigo-600 dark:fill-gray-600  transition-colors duration-200 ${pathname === "/expired" ? "!fill-indigo-500" : ""}`}
+        />
+      ),
+    },
+    {
+      id: 4,
+      label: "Add Task",
+      link: "/new",
+      icon: (
+        <MdAddTask
+          className={`h-5 w-5 group-hover:fill-indigo-600 dark:fill-gray-600  transition-colors duration-200 ${pathname === "/expired" ? "!fill-indigo-500" : ""}`}
+        />
+      ),
     },
   ];
 
   return (
-    <div className="mt-24">
-      {navList.map((item) => (
-        <NavItem
-          key={item.id}
-          label={item.label}
-          link={item.link}
-          icon={item.icon}
-        />
-      ))}
+    <div className="bg-[#1c212c] min-h-full flex flex-col items-center pt-5 pb-2 space-y-7">
+      <div className="w-full pr-3 flex flex-col gap-y-1 text-gray-500 fill-gray-500 text-sm">
+        <div className="font-QuicksandMedium pl-4 text-gray-400/60 text-xs text-[11px] uppercase">
+          Menu
+        </div>
+        {navList.map((item) => (
+          <NavItem
+            key={item.id}
+            label={item.label}
+            link={item.link}
+            icon={item.icon}
+          />
+        ))}
+      </div>
     </div>
   );
 };
